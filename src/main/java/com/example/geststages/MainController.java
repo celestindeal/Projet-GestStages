654d8e01aed5ad138e4cca2b5d0f0482cb0af946
Celestindeal <celestindeal42@gmail.com>
2023-01-13 14:47:42 +0100

    feat: layouts (doc teacher)

---
+++ src/main/java/com/example/geststages/MainController.java
@@ -0,0 +1,21 @@
+package com.example.geststages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("/products")
+    public String products() {
+        return "products";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
+}
