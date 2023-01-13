c8c5cd44e7c72202ad9a2d84c5b4cfc673ba22e3
MartinB000 <90375095+MartinB000@users.noreply.github.com>
2023-01-13 11:10:51 +0100

    INIT

---
+++ src/main/java/com/example/geststages/GestStagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.geststages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestStagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestStagesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/geststages/GestStagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geststages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestStagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
