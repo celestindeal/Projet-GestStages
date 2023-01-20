81da20e255b1bd3e593e9919da75747c1db5bf45
Celestindeal <celestindeal42@gmail.com>
2023-01-20 10:50:14 +0100

    add controleur poiur envoyer les modificationd 'entreprise

--- src/main/java/com/example/geststages/controllers/InfoEntrepriseController.java
+++ src/main/java/com/example/geststages/controllers/InfoEntrepriseController.java
@@ -3,9 +3,12 @@ package com.example.geststages.controllers;
 import com.example.geststages.models.Entreprise;
 import com.example.geststages.services.EntrepriseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,5 +27,15 @@ public class InfoEntrepriseController {
 
         model.addAttribute("entreprise", entreprise);
         return "infoEntreprise";
+    }
+
+    @PostMapping("/entreprise")
+    public String greetingSubmit(@ModelAttribute Entreprise entreprise, Model model) {
+        model.addAttribute("entreprise", entreprise);
+        System.out.println("entreprise = " + entreprise);
+
+        List<Entreprise> entreprises = entrepriseService.getAll();
+        model.addAttribute("entreprises", entreprises);
+        return "entreprise";
     }
 }
